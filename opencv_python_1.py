e125aaa42f0205862906fcbd0fee273e9a070b0e
priscilla <941705864@qq.com>
2018-10-24 13:10:18 +0800

    OpenCV show a picture

---
+++ opencv_python_1.py
@@ -0,0 +1,10 @@
+#opencv using matplotlib to show an image
+#yang hai xin 2018 10/20
+
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+im1 = cv.imread('/home/priscilla/opencv_python/dog.jpg', 0)
+plt.imshow(im1, cmap = 'gray', interpolation = 'bicubic')
+plt.xticks([]), plt.yticks([])X
+plt.show()
