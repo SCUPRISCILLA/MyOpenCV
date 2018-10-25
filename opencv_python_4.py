50a4a0a9e1be885155c667477b399d65c9476288
priscilla <941705864@qq.com>
2018-10-25 12:49:57 +0800

    using copyMakeBorder to make a border

--- opencv_python_4.py
+++ opencv_python_4.py
@@ -3,15 +3,22 @@ import numpy as np
 
 img = cv.imread('dog.jpg')
 print(img.shape)
-nose = img[40:80, 40:80]   #take a picese of image
+nose = img[40:80, 40:80]   
+#take a picese of image
 cv.imshow('nose', nose)
 
 img[:, :, 1] = 0
 img[:, :, 2] = 0
 cv.imshow('blue', img)
+#make the BGR channel's Green ang Red into 0
+#it will make the image be Blue
 
-constant = cv.copyMakeBorder(img, 100, 100, 100, 100, cv.BORDER_CONSTANT, value=[255, 0, 0])
-cv.imshow('border', constant)  #add a border to image
+constant = cv.copyMakeBorder(img, 100, 100, 100, 100, cv.BORDER_CONSTANT, value=[0, 255, 0])
+#add a border to image
+#the border is added 100 to up, 100 to buttom, 100 to right...
+#cv.BORDER_CONSTANT is one of the border interpolation(cha zhi) algorithm, value is green
+#there are so many border algorithm
+cv.imshow('border', constant)  
 
 cv.waitKey(0)
 cv.destroyAllWindows()
