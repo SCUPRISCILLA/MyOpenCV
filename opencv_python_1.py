225667321da6f82ffa30a94fd92123489269e5aa
priscilla <941705864@qq.com>
2018-10-25 12:15:56 +0800

    imread() to show a image

--- opencv_python_1.py
+++ opencv_python_1.py
@@ -4,7 +4,9 @@
 import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
+
 im1 = cv.imread('/home/priscilla/opencv_python/dog.jpg', 0)
+#using imread() function to show a image
 plt.imshow(im1, cmap = 'gray', interpolation = 'bicubic')
 plt.xticks([]), plt.yticks([])X
 plt.show()
