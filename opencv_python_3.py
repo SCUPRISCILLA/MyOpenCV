22dfe4730c4adb147023c3c8573b3af76adb5449
priscilla <941705864@qq.com>
2018-10-25 12:39:52 +0800

    image's pixel property, like col row and channel

--- opencv_python_3.py
+++ opencv_python_3.py
@@ -4,12 +4,23 @@ import cv2 as cv
 import numpy as np
 img = cv.imread('dog.jpg')
 px = img[100, 100]
-print(px)       
+#img[x, y] is to select the position x,y
+#and return this position's RGB in tuple
+print('px=', px)       
 blue = img[100, 100, 0]
-print(blue)
+#return is reverse
+#RGB but return BGR in 0 1 2
+#so img[,,0] is Blue
+print('blue=', blue)
 print(img.item(100, 100, 0))
+#return channel 0, which is blue channel
 img.itemset((100, 100, 0), 100)
+#set 100,100 channel 0 to 100
+#so img[100, 100, 0] become 100 instead of 53
 print(img.item(100, 100, 0))
 print(img.shape)
-print(img.size)  #row * col = the number of pix
-print(img.dtype) #date type uint8 etc
+#show the img's row and col and channels
+print(img.size) 
+#row * col = the number of pix
+print(img.dtype) 
+#date type uint8 etc
