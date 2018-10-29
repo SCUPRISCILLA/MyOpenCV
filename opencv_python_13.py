bf6e94cb9d1f1fa97086f19fba5af1c294f696fa
priscilla <941705864@qq.com>
2018-10-29 23:34:47 +0800

    adaptive threshold mean to make a BWimage

--- opencv_python_13.py
+++ opencv_python_13.py
@@ -2,20 +2,22 @@ import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv.imread('txbb.jpg', 0)
+img = cv.imread('book.jpg', 0)
 img = cv.medianBlur(img, 5)
 #median filter
-ret, th1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
-th2 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
-#make a 11*11 matrix to caculate threshold
-th3 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
-titles = ['Original Image', 'Global Thresholding (v = 127)',
-'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
-images = [img, th1, th2, th3]
-for i in range(0, 4):
-	plt.subplot(2,2,i+1),plt.imshow(images[i],'gray')
-	plt.title(titles[i])
-	plt.xticks([]),plt.yticks([])
-plt.show()
+hist = cv.calcHist([img], [0], None, [256], [0, 256])
+# plt.plot(hist)
+# plt.show()
+#show the hist to choose a better threshold, like 200
+ret, th1 = cv.threshold(img, 200, 255, cv.THRESH_BINARY)
+print(ret)
+#ret is the threshold
+th2 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 31, 15)
+#make a 31x31 matrix to caculte the average to be the threshold
+#the bigger the matrix, the better the BW, and the param should be bigger to reduce noise
+#if the matrix is too small, it will become a edge detection
+
+cv.imshow('book1', th1)
+cv.imshow('book2', th2)
 cv.waitKey(0)
 cv.destroyAllWindows()
