dbb4b5db741fc87f71d1b0bbe4e949068729ed72
priscilla <941705864@qq.com>
2018-10-30 22:32:11 +0800

    filter2D using kernel to be a filter

--- opencv_python_14.py
+++ opencv_python_14.py
@@ -5,14 +5,10 @@ from matplotlib import pyplot as plt
 img = cv.imread('dog.jpg')
 kernel = np.ones((5, 5), np.float32)/25
 dst = cv.filter2D(img, -1, kernel)
+#filter2D is to use kernel to do image filter
+#kernel could be guassian, average, or median
 #-1 is to make sure dst has the same deep with img
 #-1 is means dst and img are same bit of pixel
-
-# plt.subplot(121),plt.imshow(img),plt.title('Original')
-# plt.xticks([]), plt.yticks([])
-# plt.subplot(122),plt.imshow(dst),plt.title('Averaging')
-# plt.xticks([]), plt.yticks([])
-# plt.show()
 cv.imshow('res', dst)
 cv.waitKey(0)
 cv.destroyAllWindows()
