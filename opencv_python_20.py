d911416d9025e6d668ea29b6c304882d3e712887
priscilla <941705864@qq.com>
2018-11-06 13:36:20 +0800

    find the contours of image and show them on the origin img

--- opencv_python_20.py
+++ opencv_python_20.py
@@ -1,10 +1,25 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('txbb.jpg', 0)
-ret, thresh = cv.threshold(img, 127, 255, 0)
-image, contours, hierarchy = cv.findContours(thresh,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
-img1 = cv.drawContours(img, contours, 3, (0, 255, 0), 3)
-cv.imshow('res', img1)
+img = cv.imread('ContactLens.tif')
+img = cv.resize(img, (512, 512))
+imggray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+thresh = cv.adaptiveThreshold(imggray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 35, 15)
+#matrix 35*35, and parameter is 15 to do a adaptive threshold to get a BW pic
+image, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+#findContours is to find the contours, return three : image is the image of contours
+#contours is the position of contours
+#hierarchy I don't know what used for...
+print(img.shape)
+cv.drawContours(img, contours, -1, (0, 0, 255), 3)
+#to draw contours on the picture 'img'
+#-1 is to draw all contours
+#(0, 0, 255) is red
+#3 is the thin of line
+
+cv.imshow('res', img)
+cv.imshow('thre', thresh)
+
 cv.waitKey(0)
 cv.destroyAllWindows()
