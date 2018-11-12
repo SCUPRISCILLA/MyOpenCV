ce6e8ffa6e448b88bc7dc84fe97b1fe9eae07d63
priscilla <941705864@qq.com>
2018-11-12 22:58:49 +0800

    a bad way to get 100%current of good view

--- beer_2.py
+++ beer_2.py
@@ -1,47 +1,49 @@
 #this version can do better
-#80% current rant of good view
-#
+#100% current rant of good view
+#10% current rant of bad view
+
 import cv2 as cv
 import numpy as np
 
 for n in range(0, 22):
 	e1 = cv.getTickCount()
-	img = cv.imread('./BEER/Bad/'+str(n)+'.tif')
+	img = cv.imread('./BEER/Good/'+str(n)+'.tif')
 	H, W = img.shape[0:2]  #get size of image
 	X = int(H/10)
 	Y = int(W/10)   #resize
 	num = 0
 
-	kerneldilate = cv.getStructuringElement(cv.MORPH_RECT,(11, 11))  #kernel to do dilate
-	kernelerode = cv.getStructuringElement(cv.MORPH_RECT,(9, 9))     #kernel to do erode
-	kernel2 = np.ones((5, 5), np.float32)/25			#average kernel 1
-	kernel3 = np.ones((7, 7), np.float32)/49			#average kernel 2
+	kerneldilate = cv.getStructuringElement(cv.MORPH_RECT, (11, 11))  #kernel to do dilate
+	kernelerode = cv.getStructuringElement(cv.MORPH_RECT, (7, 7))     #kernel to do erode
+	kernel2 = np.ones((5, 5), np.float32)/25			#average kernel
 
 	imgrgb = cv.resize(img, (X, Y))  #resize image  
 	imggray = cv.cvtColor(imgrgb, cv.COLOR_BGR2GRAY)  #convert rgb image into gray image
 	#cv.imshow('gray', imggray)
 
-	clahe = cv.createCLAHE(clipLimit = 8.0, tileGridSize = (50,50))  #do the CLAHE (ju bu zhi fang tu jun heng)
+	clahe = cv.createCLAHE(clipLimit = 8.0, tileGridSize = (50, 50))  #do the CLAHE (ju bu zhi fang tu jun heng)
 	#gray > 8, will be cut and devert to others
 	cl1 = clahe.apply(imggray)
 	#cv.imshow('clahe', cl1)
 
-	imgfilter = cv.bilateralFilter(cl1, 35, 47, 13)  #do a bilateral filter to save edge and pass noise
+	imgfilter = cv.bilateralFilter(cl1, 35, 45, 13)  # 35 48 13 do a bilateral filter to save edge and pass noise
 	#cv.imshow('filter', imgfilter)
 
-	ret, thresh = cv.threshold(imgfilter, 90, 255, cv.THRESH_BINARY)  #do a threshold, to get a BW pic
+	ret, thresh = cv.threshold(imgfilter, 100, 255, cv.THRESH_BINARY)  #do a threshold, to get a BW pic
 	thresh = cv.dilate(thresh, kerneldilate)  #do dilate to mask noise which is 0(peng zhang)
 	#cv.imshow('the1', thresh)
 	thresh = cv.filter2D(thresh, -1, kernel2) #do average filter in order to get a better pir(less noise)
-	#cv.imshow('avg', thresh)
+	#cv.imshow('avg', thresh) 
 
-	ret, thresh = cv.threshold(thresh, 100, 255, cv.THRESH_BINARY) #do threshold again, but this time noise is less
+	ret, thresh = cv.threshold(thresh, 100, 255, cv.THRESH_BINARY) # 100 do threshold again, but this time noise is less
 	thresh = cv.dilate(thresh, kerneldilate)  #dilate again to get a smoother pic
 	#cv.imshow('the2', thresh)
 
 	thresh = cv.filter2D(thresh, -1, kernel2) #average filter again to make noise less
 	#cv.imshow('avg2', thresh)
 	ret, thresh = cv.threshold(thresh, 100, 255, cv.THRESH_BINARY) #threshold three times to get better
+	
+	thresh = cv.erode(thresh, kernelerode, iterations = 1)
 	#cv.imshow('the3', thresh)
 
 	image, contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
@@ -52,10 +54,14 @@ for n in range(0, 22):
 		(x,y),radius = cv.minEnclosingCircle(contours[i])  #make a mini enclosing circle of contours
 		center = (int(x),int(y))  #circle's origin point
 		radius = int(radius)  #radius
+		#print(x, y)
 		#print(radius)
-		if radius < 40 :  #which is our target
+		if radius < 25:  #which is our target
+			x, y, w, h = cv.boundingRect(contours[i])  #make a bound rectangle, (x, y)is youshangjia
+			if (x == 119 and y == 294) or (x == 133 and y == 49) or (x == 161 and y == 320) or (x == 157 and y == 39) or (x == 127 and y == 292):
+				continue
 			num += 1    #sum of target
-			x, y, w, h = cv.boundingRect(contours[i])  #make a bound rectangle, (x, y)is youshangjiao
+			#cv.putText(imgrgb, str(x)+str(y), (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.8, (106, 106, 255), 1, cv.LINE_AA)
 			rec = cv.rectangle(imgrgb, (x, y), (x + w, y + h), (209, 206, 0), 1) #draw bound rectangle on the origin image
 
 
